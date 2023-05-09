2cc429b169c3b1e3ea9b38bff5f6441ad35d0a81
Xtrialandtest <1trialandtest@gmail.com>
2023-05-09 11:16:34 +0300

    booststrap added

--- WeeklyProjects/settings.py
+++ WeeklyProjects/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'caloriesCounter',
-    "bootstrap4"
+    "bootstrap4",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,8 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
